265388687bc53ce6a34bf5bbe732f68d8554a221
AdeedKhan <Muhammadadeedk@gmail.com>
2024-07-22 20:07:23 +0500

    initial commit

---
+++ collection/Collection/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Collection
+
+{
+    using System.Collections;
+    using System.Collections.Generic;
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello, World!");
+            ArrayList arrayList = new ArrayList();
+            arrayList.Add(1);
+            arrayList.Add("Hello");
+            arrayList.Add(true);
+            foreach (var o in arrayList)
+                Console.WriteLine(o);
+        }
+    }
+}
